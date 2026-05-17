b6f25963567f90cd1678b9245ff24f41d3a8fd41
Nikolas Dahn <nikolas.dahn@gmail.com>
2026-05-17 21:09:54 +0200

    Will now keep the terminal open after exit

--- aet_collector.py
+++ aet_collector.py
@@ -78,111 +78,114 @@ def save_config(config: dict = None, **overrides) -> None:
 
 
 if __name__ == "__main__":
-    p = argparse.ArgumentParser()
-    p.add_argument("matbins", nargs="+", type=Path, help="Paths to one or more matbin files")
-    group = p.add_mutually_exclusive_group()
-    group.add_argument(
-        "-g",
-        "--game",
-        choices=["NR", "ER", "DS3", "SDT"],
-        help="Use this game for locating AETs",
-    )
-    group.add_argument(
-        "-p",
-        "--game-path",
-        type=Path,
-        help="Manually set the game path to locate AETs. If neither game nor game-path are provided the game path is determined from the matbin location if possible.",
-    )
-    p.add_argument(
-        "-o",
-        "--output",
-        type=Path,
-        required=False,
-        help="Where to save located AETs. Will default to an 'aet' folder next to the matbin.",
-    )
-
-    args = p.parse_args()
-
-    # === game path from game type via config ===
-    if args.game:
-        game = args.game = args.game.upper()
-        if game == "NR":
-            cfg = GET_CONFIG()
-            if "NIGHTREIGN_PATH" in cfg:
-                args.game_path = cfg["NIGHTREIGN_PATH"]
-            else:
-                args.game_path = DEFAULT_NIGHTREIGN_PATH
-        elif game == "ER":
-            args.game_path = ELDEN_RING_PATH
-        elif game == "DS3":
-            args.game_path = DS3_PATH
-        elif game == "SDT":
-            args.game_path = SEKIRO_PATH
-        else:
-            raise ValueError(f"Invalid game {game}")
-
-    # === game path from matbin path ===
-    if not args.game_path or not args.game_path.is_dir():
-        # Try to determine the game directory from the matbin path first
-        parent: Path = args.matbins[0].parent
-        while parent and parent.parent != parent:
-            candidate = parent / AET_BASE_DIR
-            if candidate.is_dir():
-                args.game_path = parent
-                break
-
-            parent = parent.parent
-        else:
-            input(
-                "Failed to determine game directory, please press Enter and choose the game's .exe manually..."
-            )
-            ret = open_file("Locate Game .exe", str(args.matbins[0].parent), ".exe")
-            if ret:
-                args.game_path = Path(ret).parent
-
-    if not args.game_path:
-        raise ValueError(
-            "Could not determine game and neither game nor game-path were provided"
+    try:
+        p = argparse.ArgumentParser()
+        p.add_argument("matbins", nargs="+", type=Path, help="Paths to one or more matbin files")
+        group = p.add_mutually_exclusive_group()
+        group.add_argument(
+            "-g",
+            "--game",
+            choices=["NR", "ER", "DS3", "SDT"],
+            help="Use this game for locating AETs",
+        )
+        group.add_argument(
+            "-p",
+            "--game-path",
+            type=Path,
+            help="Manually set the game path to locate AETs. If neither game nor game-path are provided the game path is determined from the matbin location if possible.",
+        )
+        p.add_argument(
+            "-o",
+            "--output",
+            type=Path,
+            required=False,
+            help="Where to save located AETs. Will default to an 'aet' folder next to the matbin.",
         )
 
-    # Use game folder if the game exe was provided
-    if args.game_path.is_file():
-        args.game_path = args.game_path.parent
+        args = p.parse_args()
 
-    if not (args.game_path / AET_BASE_DIR).is_dir():
-        raise ValueError(f"Game directory does not contain {AET_BASE_DIR} subfolder")
+        # === game path from game type via config ===
+        if args.game:
+            game = args.game = args.game.upper()
+            if game == "NR":
+                cfg = GET_CONFIG()
+                if "NIGHTREIGN_PATH" in cfg:
+                    args.game_path = cfg["NIGHTREIGN_PATH"]
+                else:
+                    args.game_path = DEFAULT_NIGHTREIGN_PATH
+            elif game == "ER":
+                args.game_path = ELDEN_RING_PATH
+            elif game == "DS3":
+                args.game_path = DS3_PATH
+            elif game == "SDT":
+                args.game_path = SEKIRO_PATH
+            else:
+                raise ValueError(f"Invalid game {game}")
 
-    # === game type from
-    if not args.game:
-        path = args.game_path
-
-        if (path / "nightreign.exe").is_file():
-            args.game = "NR"
-        else:
-            for game in GAMES:
-                if (path / game.executable_name).is_file():
-                    args.game = game.abbreviated_name.upper()
+        # === game path from matbin path ===
+        if not args.game_path or not args.game_path.is_dir():
+            # Try to determine the game directory from the matbin path first
+            parent: Path = args.matbins[0].parent
+            while parent and parent.parent != parent:
+                candidate = parent / AET_BASE_DIR
+                if candidate.is_dir():
+                    args.game_path = parent
                     break
 
-    # === save back to config if we managed to locate the game dir ===
-    if args.game:
-        key = {
-            "NR": "NIGHTREIGN_PATH",
-            "ER": "ELDEN_RING_PATH",
-            "DS3": "DS3_PATH",
-            "SDT": "SEKIRO_PATH",
-        }[args.game]
-        
-        cfg = GET_CONFIG()
-        cfg[key] = str(args.game_path)
-        save_config(cfg)
-    else:
-        print("WARNING: could not determine game type, config has not been updated")
+                parent = parent.parent
+            else:
+                input(
+                    "Failed to determine game directory, please press Enter and choose the game's .exe manually..."
+                )
+                ret = open_file("Locate Game .exe", str(args.matbins[0].parent), ".exe")
+                if ret:
+                    args.game_path = Path(ret).parent
 
-    # default output dir
-    if not args.output:
-        args.output = Path(sys.executable).parent / "aet"
-    args.output.mkdir(parents=True, exist_ok=True)
+        if not args.game_path:
+            raise ValueError(
+                "Could not determine game and neither game nor game-path were provided"
+            )
 
-    for mat in args.matbins:
-        collect_aets(mat, args.game_path, args.output)
+        # Use game folder if the game exe was provided
+        if args.game_path.is_file():
+            args.game_path = args.game_path.parent
+
+        if not (args.game_path / AET_BASE_DIR).is_dir():
+            raise ValueError(f"Game directory does not contain {AET_BASE_DIR} subfolder")
+
+        # === game type from
+        if not args.game:
+            path = args.game_path
+
+            if (path / "nightreign.exe").is_file():
+                args.game = "NR"
+            else:
+                for game in GAMES:
+                    if (path / game.executable_name).is_file():
+                        args.game = game.abbreviated_name.upper()
+                        break
+
+        # === save back to config if we managed to locate the game dir ===
+        if args.game:
+            key = {
+                "NR": "NIGHTREIGN_PATH",
+                "ER": "ELDEN_RING_PATH",
+                "DS3": "DS3_PATH",
+                "SDT": "SEKIRO_PATH",
+            }[args.game]
+            
+            cfg = GET_CONFIG()
+            cfg[key] = str(args.game_path)
+            save_config(cfg)
+        else:
+            print("WARNING: could not determine game type, config has not been updated")
+
+        # default output dir
+        if not args.output:
+            args.output = Path(sys.executable).parent / "aet"
+        args.output.mkdir(parents=True, exist_ok=True)
+
+        for mat in args.matbins:
+            collect_aets(mat, args.game_path, args.output)
+    finally:
+        input("Press enter to exit...")
